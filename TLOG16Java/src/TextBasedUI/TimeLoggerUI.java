d77264282ffb3b072bbc896713962c2b3eda5fd7
stamie <estampel@gmail.com>
2018-12-13 20:01:59 +0100

    save in night

--- TLOG16Java/src/TextBasedUI/TimeLoggerUI.java
+++ TLOG16Java/src/TextBasedUI/TimeLoggerUI.java
@@ -9,7 +9,7 @@ import Logger.Task;
 import Logger.WorkDay;
 import Logger.WorkMonth;
 import Logger.TimeLogger;
-import TextBasedUI.Scanner;
+
 
 /**
  *
@@ -40,45 +40,49 @@ ask for end time (format: 12:45, with validation)
 8. Delete a task: ask for month, day, select task - ask for confirmation!
 9. Modify task: ask for month, day, task, let change every fields (shows previous value in braces, if the input is empty, don't change the value!)
 10. Statistics: ask for month, then print the statistics of the month, and the statistics of the days of this month
-*/
+ */
 public class TimeLoggerUI {
-    
-    public TimeLoggerUI(int input, TimeLogger timeLogger){
-        
+
+    public boolean TimeLoggerUICreat(int input, TimeLogger timeLogger) {
+
         switch (input) {
-    
+
             case 0: // Exit
-                return;
+                return false;
             case 1: // List months: shows a counter, the year & the month line by line (example: 1. 2016-09, 2. 2016-10, ...)
                 listMonths(timeLogger);
                 break;
-            case 2: /* List days:
+            case 2:
+                /* List days:
 lists the months, select one by ask for row number
 list all workdays of this month */
                 listDays(timeLogger);
                 break;
-                
+
             case 3: // List tasks for a specific day (ask for month & day)
                 listTasks(timeLogger);
                 break;
             case 4: // Add new month: specify year & month with integers
                 addNewMonth(timeLogger);
                 break;
-            case 5: /* Add day to a specific month:
+            case 5:
+                /* Add day to a specific month:
 list the workmonths (2. menu item)
 ask the index of workmonth
 ask the day
 ask the required working hours, default value=7.5 */
                 addDay(timeLogger);
                 break;
-            case 6:  /* Start a task for a day
+            case 6:
+                /* Start a task for a day
 ask for month, day, task id, what you do (comment)
 ask for start time in format 10:30 
 if there is a task in the day, get the end time of the last task and show it in braces! If the user enters an empty value, save that time in the task!
 don't ask for the end time! */
                 startTask(timeLogger);
                 break;
-            case 7: /* Finish a specific task: 
+            case 7:
+                /* Finish a specific task: 
 ask for month & day, 
 display only unfinished tasks
 ask for end time (format: 12:45, with validation)*/
@@ -97,51 +101,54 @@ ask for end time (format: 12:45, with validation)*/
                 System.out.println("Wrong function key!");
                 break;
         }
-    
+
+        return true;
     }
-    
-    public void listMonths(TimeLogger timeLogger){
-        
-        timeLogger.listMonths();
+
+    public void listMonths(TimeLogger timeLogger) {
+
+        System.out.println("List of month:");
+        if (!timeLogger.listMonths())
+            System.out.println("No items");
+        System.out.println();
         return;
-        
+
     }
-    
+
     public void listDays(TimeLogger timeLogger) {
-        
-        
+
     }
-    
-    public void listTasks(TimeLogger timeLogger){
-    
+
+    public void listTasks(TimeLogger timeLogger) {
+
     }
-    
+
     public void addNewMonth(TimeLogger timeLogger) {
-        
+
     }
-    
+
     public void addDay(TimeLogger timeLogger) {
-        
+
     }
-    
+
     public void startTask(TimeLogger timeLogger) {
-        
+
     }
-    
+
     public void finishASpecificTask(TimeLogger timeLogger) {
-        
+
     }
-    
+
     public void deleteTask(TimeLogger timeLogger) {
-    
+
     }
-    
+
     public void modifyTask(TimeLogger timeLogger) {
-        
+
     }
-    
+
     public void statistics(TimeLogger timeLogger) {
-        
+
     }
-    
+
 }
