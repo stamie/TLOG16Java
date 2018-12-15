d4e2ad2fbab7beb8661fe15595237a48043778c4
stamie <estampel@gmail.com>
2018-12-15 11:04:43 +0100

    repaire TimeLogger class

--- TLOG16Java/src/Logger/TimeLogger.java
+++ TLOG16Java/src/Logger/TimeLogger.java
@@ -14,79 +14,92 @@ import java.util.ArrayList;
  * @author stampel
  */
 public class TimeLogger {
-    
+
     private List<WorkMonth> months;
-    
+
     public TimeLogger(List<WorkMonth> monthsI) {
-        
+
         this.months = new ArrayList();
         this.months = monthsI;
-        
+
     }
+
     public TimeLogger() {
-        
+
         this.months = new ArrayList();
         //this.months = monthsI;
-        
+
     }
-    
-    
-/*
+
+    /*
  * getter for months
- */
+     */
     public List<WorkMonth> getMonths() {
-        
+
         return this.months;
-        
+
     }
+
     /*
  * List for months
- */
+     */
     public boolean listMonths() {
-        
+
         int i = 1;
-     
-        for(WorkMonth month: this.months) {
-            
-            System.out.print(i + ". " + month.getDate().getYear() + "-" + month.getDate().getMonth() + ", ");
+
+        for (WorkMonth month : this.months) {
+
+            System.out.print(i + ". " + month.getDate().getYear() + "-" + month.getDate().getMonthValue() + ", ");
             i++;
-            
+
         }
-        if (i > 1)
-        return true;
+        if (i > 1) {
+            return true;
+        }
         return false;
     }
-/*
+
+    /*
  * boolean isNewMonth(WorkMonth):
  * boolean decides, if this month already exists or not
- */
-    
+     */
     public boolean isNewMonth(WorkMonth wm) {
-        
-        for (WorkMonth wm2: this.months) {
+
+        for (WorkMonth wm2 : this.months) {
             if (wm2.getDate() == wm.getDate()) {
                 return false;
             }
         }
         return true;
-    
-    
-    
-    }
-/*
- * void addMonth(WorkMonth):
- * void  adds a new month to the months list if it is new
- */
-    public void addMonth(WorkMonth wm) {
-        
-        if (this.isNewMonth(wm)) {
-            
-            this.months.add(wm);
-        
-        }
-        
+
     }
 
-    
-    
+    /*
+ * void addMonth(WorkMonth):
+ * void  adds a new month to the months list if it is new
+     */
+    public void addMonth(WorkMonth wm) {
+
+        if (this.isNewMonth(wm)) {
+
+            if (this.months.isEmpty()) {
+                this.months.add(wm);
+                return;
+            }
+            int i = -1;
+
+            for (WorkMonth month : this.months) {
+                i++;
+                if ((month.getDate().getMonthValue() > wm.getDate().getMonthValue() && month.getDate().getYear() == wm.getDate().getYear())
+                        || (month.getDate().getYear() > wm.getDate().getYear())) {
+                    this.months.add(i, wm);
+                    return;
+                }
+            }
+            this.months.add(wm);
+
+        }
+
+    }
+
 }
